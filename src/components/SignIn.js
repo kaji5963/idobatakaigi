d47c3f65c8d859484b42c373650bfc22118cb31f
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-13 18:06:45 +0900

    update2

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -61,11 +61,14 @@ export default function SignIn({ setName }) {
           />
 
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
             disabled={disabled}
+            onClick={() => {
+              setName(string);
+            }}
           >
             はじめる
           </Button>
