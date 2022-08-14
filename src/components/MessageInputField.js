2bf37ca161023ff9ab8a31ce348431dd22bde834
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-14 09:44:24 +0900

    update10

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Avatar, Grid } from "@material-ui/core";
 import { gravatarPath } from "../gravatar";
+import MessageField from "./MessageField";
 
 const useStyles = makeStyles({
   root: {
@@ -11,6 +12,7 @@ const useStyles = makeStyles({
 });
 
 const MessageInputField = ({ name }) => {
+  const [text, setText] = useState("");
   const classes = useStyles();
   const avatarPath = gravatarPath(name);
 
@@ -21,7 +23,7 @@ const MessageInputField = ({ name }) => {
           <Avatar src={avatarPath} />
         </Grid>
         <Grid item xs={10}>
-          入力
+          <MessageField name={name} setText={setText} text={text} />
         </Grid>
         <Grid item xs={1}>
           ボタン
