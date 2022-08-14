72a89e89777dae652aebcfdcf17dcb44fbcd2347
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-14 11:45:05 +0900

    update12

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,27 @@
 import firebase from "firebase";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTO_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCwJ3s8W6plz-irw1o6vfXptEs6hTc8d9w",
-  authDomain: "idobatakaigi-tool.firebaseapp.com",
-  projectId: "idobatakaigi-tool",
-  storageBucket: "idobatakaigi-tool.appspot.com",
-  messagingSenderId: "826119711247",
-  appId: "1:826119711247:web:90283fc62037dc0628517a",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTO_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref("messages");
+const messagesRef = database.ref("messages");
 
 export const pushMessage = ({ name, text }) => {
-  messageRef.push({ name, text });
+  messagesRef.push({ name, text });
 };
